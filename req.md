16e4d5f0a034ec12064d585117163e53d02020d4
dot_ <muriloholiveira@hotmail.com>
2022-10-25 08:13:47 -0300

    :lipstick: Adding SASS initial files

---
+++ req.md
@@ -0,0 +1,3 @@
+Random passwords based on some users configs/selectors
+Encrypted passwords based on user's passphrase
+Download passphrase in an txt file.
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import "./styles/home.css"
 
 function App() {
   return (
-    <div className="App">
-      Test
+    <div className="container">
+      <h1>Password Generator</h1>
+      <section className="options"></section>
+      <section className="content"></section>
     </div>
   );
 }
