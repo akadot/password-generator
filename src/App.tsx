ca0f87de7d2f2d74c5ccc373861df83e0147a11a
Murilo Henrique de Oliveira <lab3.sp.prnf3regiao@pgfn.gov.br>
2022-10-25 13:46:21 -0300

    :lipstick: Styling home page

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,20 @@ function App() {
   return (
     <div className="container">
       <h1>Password Generator</h1>
-      <section className="options"></section>
-      <section className="content"></section>
+      <section className="options">
+        <button>Random</button>
+        <button>Encrypted</button>
+      </section>
+      <section className="content">
+        <p>1sad123SD</p>
+        <label htmlFor="numbers">Numbers</label>
+        <input type="checkbox" name="" id="numbers" />
+        <label htmlFor="upper">Upper Case</label>
+        <input type="checkbox" name="" id="upper" />
+        <label htmlFor="symbols">Symbols</label>
+        <input type="checkbox" name="" id="symbols" />
+        <button>Generate</button>
+      </section>
     </div>
   );
 }
