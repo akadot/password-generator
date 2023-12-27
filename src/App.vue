8cad9f371f499df086bb08e54cc16cdb69afb3b6
akadot <muriloholiveira@hotmail.com>
2023-12-27 20:41:44 -0300

    :tada: Initializing vite/vue setup

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="container">
+    <section class="content">
+      <h1>dotpass</h1>
+      <h2>Slogan</h2>
+      <p>Mussum Ipsum, cacilds vidis litro abertis.  Aenean aliquam molestie leo, vitae iaculis nisl. Cevadis im ampola pa arma uma pindureta. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis! Casamentiss faiz malandris se pirulitá.</p>
+      <a href="#">To keep multiple passwords, create account.</a>
+    </section>
+    <section>
+      <p>component</p>
+    </section>
+  </section>
+</template>
+
+<style scoped>
+.container{
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
+  padding: 0 10rem; 
+}
+
+.content{
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  word-wrap: break-word;
+  width: 40%;
+}
+
+</style>
